91f8e42c2813710e197707723e24f8309f6a0004
cloverfield344 <zhoomartov344@mail.ru>
2020-09-29 06:52:28 +0300

    First commit

---
+++ myFirstProject/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def about(request):
+    return HttpResponse('This is about page')
+
+
+def home(request):
+    return render(request, 'home.html', {'greeting': 'Hello'})
+
+
+def vice_versa(request):
+    return render(request, 'test.html')
